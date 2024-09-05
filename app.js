71c7f7bbb01e36b087d3f9076654de373c61bec5
yusekim <youwin0802@naver.com>
2024-09-05 16:43:07 +0900

    basic JS learned variables

---
+++ app.js
@@ -0,0 +1,16 @@
+let a = 6
+const b = 2
+let myName = "Yuseung"
+
+// var c = 0 거의 안쓰고 권장하지 않음
+// let 은 변수, const 는 상수
+// a = 3 문제없음
+// b = 9 오류
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log("Hello " + myName);
+
+myName = "Yusekim"
+console.log("NewName is " + myName);
